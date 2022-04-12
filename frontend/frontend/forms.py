bc29df16677984c3b75cfff102e00da35c0bd36b
vishruthdevan <vishruthdevan@gmail.com>
2022-04-12 21:50:03 +0530

    feat: implement auth system

--- frontend/frontend/forms.py
+++ frontend/frontend/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from frontend.models import Admin
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -11,6 +11,10 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        admin = Admin.query.filter_by(username=username.data).first()
+        if admin:
+            raise ValidationError('That username is taken. Please choose a different one.')
 
 class LoginForm(FlaskForm):
     username = StringField('Username',
