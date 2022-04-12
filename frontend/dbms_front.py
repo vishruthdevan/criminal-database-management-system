ff639f78b6b3f92b55b68257c591d258e02847fd
vishruthdevan <vishruthdevan@gmail.com>
2022-04-12 09:22:06 +0530

    feat: set up sqlalchemy to query the database

--- frontend/dbms_front.py
+++ frontend/dbms_front.py
@@ -1,14 +1,41 @@
 from flask import Flask, render_template, url_for, redirect, flash
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+import pymysql
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secret123'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/project'
+db = SQLAlchemy(app)
+
+connection = db.engine.connect()
+
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"Admin('{self.username}', '{self.email}', '{self.image_file}')"
 
 
 @app.route('/')
 def home():
+    # connection.execute(text(
+    #     r'insert into victim VALUES (2, "asd", "asd", 24, "male", 802374234, 3243511)'))
+    result = connection.execute(text("SELECT * FROM victim"))
+    print(result.mappings().all())
     return render_template('index.html')
+
+
+@app.route('/db')
+def db():
+    result = connection.execute(text("SELECT * FROM victim"))
+    result = result.mappings().all()
+    return render_template('db.html', result=result)
 
 
 @app.route("/register", methods=['GET', 'POST'])
